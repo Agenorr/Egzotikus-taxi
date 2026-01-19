eaabe898b9d2ca1dd719cdbd78b64c3d4b64bd12
Futó Ábel <gamelife9222@gmail.com>
2026-01-19 23:48:14 +0100

    Login api

--- obj/Debug/net8.0/ExoticBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/ExoticBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExoticBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9c28acab0b93a021a3bac016cd0166677af8228")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+374c2364125bda8106055bec1b26732950f4e061")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExoticBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExoticBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
